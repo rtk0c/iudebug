efdd1359c2949dfcd42784df802a39294932fa00
rtk0c <mail.tianyig@gmail.com>
2022-06-28 03:14:23 +0000

    Change ImGui to docking branch

--- source/main.cpp
+++ source/main.cpp
@@ -4,45 +4,41 @@
 #include <GLFW/glfw3.h>
 
 #include <glad/glad.h>
-#include <iostream>
+#include <cstdio>
+#include <cstdlib>
 #include <string>
 
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
 
-#define IMGUI_IMPL_OPENGL_LOADER_CUSTOM 1
-#include "imgui_impl_glfw.inl"
-#include "imgui_impl_opengl3.inl"
-
 static void GlfwErrorCallback(int error, const char* description) {
     fprintf(stderr, "Glfw Error %d: %s\n", error, description);
 }
 
 int main() {
+    glfwSetErrorCallback(&GlfwErrorCallback);
     if (!glfwInit()) {
         return -1;
-    }
-    
-    glfwSetErrorCallback(&GlfwErrorCallback);
+    }    
     
     // Decide GL+GLSL versions
 #if defined(IMGUI_IMPL_OPENGL_ES2)
     // GL ES 2.0 + GLSL 100
-    const char* glsl_version = "#version 100";
+    const char* glslVersion = "#version 100";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
 #elif defined(__APPLE__)
     // GL 3.2 + GLSL 150
-    const char* glsl_version = "#version 150";
+    const char* glslVersion = "#version 150";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // 3.2+ only
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Required on Mac
 #else
     // GL 3.0 + GLSL 130
-    const char* glsl_version = "#version 130";
+    const char* glslVersion = "#version 130";
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
 #endif
@@ -60,10 +56,17 @@ int main() {
     
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
+
+    auto& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    //io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+    //io.ConfigViewportsNoAutoMerge = true;
+    //io.ConfigViewportsNoTaskBarIcon = true;
     
     ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init(glsl_version);
+    ImGui_ImplOpenGL3_Init(glslVersion);
     
+    ImVec4 clearColor(0.45f, 0.55f, 0.60f, 1.00f);
     App app;
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -75,14 +78,20 @@ int main() {
         app.Show();
         
         ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        auto clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-        glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w);
+        int dispalyWidth, displayHeight;
+        glfwGetFramebufferSize(window, &dispalyWidth, &displayHeight);
+        glViewport(0, 0, dispalyWidth, displayHeight);
+        glClearColor(clearColor.x * clearColor.w, clearColor.y * clearColor.w, clearColor.z * clearColor.w, clearColor.w);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
         
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+            GLFWwindow* backupCurrentCtx = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backupCurrentCtx);
+        }
+
         glfwSwapBuffers(window);
     }
     
